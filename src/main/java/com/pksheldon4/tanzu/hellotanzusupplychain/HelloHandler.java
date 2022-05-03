91f9630ece465617d10b4f53a78e06aa1ce0a332
Preston Sheldon <pksiv@hotmail.com>
2022-05-03 12:42:56 -0400

    Update hello message

--- src/main/java/com/pksheldon4/tanzu/hellotanzusupplychain/HelloHandler.java
+++ src/main/java/com/pksheldon4/tanzu/hellotanzusupplychain/HelloHandler.java
@@ -13,14 +13,6 @@ import static java.lang.Thread.*;
 public class HelloHandler {
 
     public Mono<ServerResponse> sayHello(ServerRequest request) {
-        try {
-            long delay = Math.round(Math.random() * 3000);
-            sleep(delay);
-            System.out.printf("Delaying: %s milliseconds\n", delay);
-            return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromValue("Hello Tanzu Supply Chain -  Update to include random delay (in milliseconds): " + delay));
-        } catch (InterruptedException ie) {
-            //Ignore
-        }
-        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromValue("Why did I get here"));
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromValue("Hello Tanzu Supply Chain - Update 3"));
     }
 }
